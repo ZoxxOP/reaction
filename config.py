6aeffe35bafb0f8baee3f02da8e6b3ec79cb743b
Badnam019 <yadavxd746@gmail.com>
2024-12-21 08:54:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ API_HASH = "580e1a3e7d0719816feebf595c9166d2"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "6726372149"))
-SUPPORT_GRP = "@FRIENDSHUBCHATZONE"
-UPDATE_CHNL = "ll_BOTCHAMBER_ll"
-OWNER_USERNAME = "ll_RADHE7_ll"
+OWNER_ID = int(getenv("OWNER_ID", "6315125023"))
+SUPPORT_GRP = "@SYSTEM_SUPPROT_GROUP"
+UPDATE_CHNL = "SYSTEM_SUPPROT_GROUP"
+OWNER_USERNAME = "lll_BADNAM_BABY_lll"
     
